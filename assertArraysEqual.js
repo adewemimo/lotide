a16e830396b0a5c2f482d65ef35a1a5e59122b52
Seun Soetan <sesoetan@gmail.com>
2021-11-11 18:53:32 -0500

    assert function to test identical arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+
+const assertArraysEqual = function(actualArray, expectedArray) {
+    if (eqArrays(actualArray, expectedArray)) {
+      console.log(`✅✅✅ Assertion Passed: ${actualArray} === ${expectedArray}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
+    } 
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1], [1]);
+assertArraysEqual([], []);
