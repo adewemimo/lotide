db1503fe24afbde7a69a6d35464ead63d3588887
Seun Soetan <sesoetan@gmail.com>
2021-11-11 18:55:06 -0500

    apply eslint to code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,22 @@
 const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
+  }
+  return true;
+};
 
 
 const assertArraysEqual = function(actualArray, expectedArray) {
-    if (eqArrays(actualArray, expectedArray)) {
-      console.log(`✅✅✅ Assertion Passed: ${actualArray} === ${expectedArray}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
-    } 
+  if (eqArrays(actualArray, expectedArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArray} === ${expectedArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
+  }
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
