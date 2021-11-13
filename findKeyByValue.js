015b757801f8664fd804898a50e31d411bc196d7
Seun Soetan <sesoetan@gmail.com>
2021-11-12 21:01:33 -0500

    function to find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+//Assertion Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(inputObject, value) {
+  for (const key of Object.keys(inputObject)) {
+    if (inputObject[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+//test cases for the function - findKeyByValue
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const ingredient = {
+  meat: "chicken",
+  vegetables: "lettuce",
+  sauce: "tomato",
+  cheese: "mozzarella",
+};
+assertEqual(findKeyByValue(ingredient, "mozzarella"), "cheese");
+assertEqual(findKeyByValue(ingredient, "chicken"), "meat");
+assertEqual(findKeyByValue(ingredient, "cabbage"), undefined);
